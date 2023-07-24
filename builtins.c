fdd44553527e79b264521bcb3c4538670c90db19
PeshO2K <patienceotuke@gmail.com>
2023-07-24 05:25:25 -0700

    Removed global builtins list

--- builtins.c
+++ builtins.c
@@ -1,16 +1,4 @@
 #include "main.h"
-
-builtin_t builtins[] = {
-	{"exit", _exit},
-	{"env", _env},
-	{"cd", _cd},
-	{"alias", _alias},
-	{"setenv", _setenv},
-	{"unsetenv", _unsetenv}
-	{NULL, NULL}
-
-};
-
 int _exit(char **ag)
 {
 	int exit_status = 0;
@@ -32,18 +20,27 @@ int _env(char **ag)
 		write(STDOUT_FILENO, *env, strlen(*env));
 		write(STDOUT_FILENO, "\n", 1);
 	}
-	
+	return (1);
 }
 
 int execute_builtin(char **ag)
 {
+	builtin_t builtins[] = {
+		{"exit", _exit},
+		{"env", _env},
+		{"cd", _cd},
+		{"alias", _alias},
+		{"setenv", _setenv},
+		{"unsetenv", _unsetenv},
+		{NULL, NULL}
+};
 	int i = 0;
 
 	for (; builtins[i].name; i++)
 	{
 		if (strcmp(ag[0], builtins[i].name) == 0)
 		{
-			return(*builtins[i].func)(ag);
+			return ((*builtins[i].func)(ag));
 		}
 	}
 	return (-1);
