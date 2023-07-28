857997eead97b7fa5f909a13b3efff7523882f2c
olaidris <olasunkanmiidris15@gmail.com>
2023-07-28 04:32:53 +0100

    Added the non interactive mode code to the main.c file

---
+++ getline.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * my_getline - Read a line from a file or a stream.
+ * @store: Double pointer to store the read line.
+ * @input_len: Pointer to the length of the buffer.
+ * @stream: Pointer to the input stream (e.g., file pointer).
+ *
+ * Return: Number of characters read, including the newline character,
+ *         or -1 if an error occurs or the end of the file is reached.
+ */
+ssize_t my_getline(char **store, size_t *input_len, FILE *stream)
+{
+	char *newline;
+	*input_len = 1024;
+
+	if (input_len == NULL || stream == NULL)
+	{
+		fprintf(stderr, "Invalid parameters\n");
+		return (-1);
+	}
+
+	*store = malloc(sizeof(char) * (*input_len));
+
+	if (*store == NULL)
+	{
+		perror("memory allocation failed");
+		return (-1);
+	}
+
+	if (fgets(*store, *input_len, stream) == NULL)
+	{
+		free(*store);
+		return (-1);
+	}
+	*input_len = strlen(*store);
+
+	*store = realloc(*store, sizeof(char) * (*input_len + 1));
+	if (*store == NULL)
+	{
+		perror("memory allocation failed");
+		return (-1);
+	}
+
+	/*char *newline;*/
+
+	newline = strchr(*store, '\n');
+
+	if (newline != NULL)
+		*newline = '\0';
+
+	return ((ssize_t)*input_len);
+}
--- main.c
+++ main.c
@@ -5,33 +5,52 @@
  * @ag: argument vector
  * Return: 0 Success
  */
-int main(int ac  __attribute__((unused)), char **ag __attribute__((unused)))
+int main(int ac __attribute__((unused)), char **ag __attribute__((unused)))
 {
 	char *line, *filepath = NULL;
 	size_t line_size = 0;
 	char **args;
-	int int_mode, i = 0, status = 1;/* i;*/
+	int int_mode, i = 0, status = 1; /* i;*/
+	FILE *fp;
 
-	while (status)
+	int_mode = isatty(STDIN_FILENO);
+	if (int_mode)
 	{
-		int_mode = isatty(STDIN_FILENO);
-		if (int_mode)
+		while (status)
 		{
 			printf("# ");
-		}
-		/*printf("loop: %d\n",i);*/
-		my_getline(&line, &line_size, stdin);
-		if (feof(stdin))
-		{
-			break;
-		}
-		args = parse(line, DELIM);
-		status = execute_cmd(args, environ);
+			/*printf("loop: %d\n",i);*/
+			my_getline(&line, &line_size, stdin);
+			if (feof(stdin))
+			{
+				break;
+			}
+			args = parse(line, DELIM);
+			status = execute_cmd(args, environ);
 
-		/*free(line);*/
-		free(filepath);
-		free(args);
-		i++;
+			/*free(line);*/
+			free(filepath);
+			free(args);
+			i++;
+		}
+	}
+	else
+	{
+		while (my_getline(&line, &line_size, stdin) != -1)
+		{
+			line[strcspn(line, "\n")] = 0;
+			fp = popen(line, "r");
+			if (fp == NULL)
+			{
+				perror("Failed to run command");
+				exit(1);
+			}
+			while (my_getline(&line, &line_size, fp) != -1)
+			{
+				printf("%s\n", line);
+			}
+			pclose(fp);
+		}
 	}
 	return (0);
 }
